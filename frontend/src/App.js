29e7e27f11453f1a41be4a8f0615d0474c8e1947
Parth Tyagi <parthtyagicode@gmail.com>
2022-12-27 14:01:12 +0530

    created nav component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Login from './pages/auth/login';
 import Registration from './pages/auth/register';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       {/* <Login /> */}
       {/* <Registration /> */}
+
     </>
   );
 }
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Navbar.css';
+import { VscAccount } from 'react-icons/vsc';
+import { useState } from 'react';
+
+export default function Navbar() {
+
+    const [showAccountInfo, setShowAccountInfo] = useState(false);
+
+    const accountInfoHandler = () => {
+        setShowAccountInfo(!showAccountInfo);
+    }
+
+    return (
+        <nav>
+            <div className="nav-logo">
+                <h1>The Written Journey</h1>
+            </div>
+
+            <div className="nav-pages">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Create</a></li>
+                </ul>
+            </div>
+
+            <div className="nav-account">
+                <VscAccount id="account-logo" onClick={accountInfoHandler} />
+
+                {showAccountInfo &&
+                    <div className="account-info">
+                        <ul>
+                            <li>My Blogs</li>
+                            <li>Published Blogs</li>
+                            <li>Un-Published Blogs</li>
+                            <li>Logout</li>
+                        </ul>
+                    </div>
+                }
+
+            </div>
+        </nav>
+    )
+}
