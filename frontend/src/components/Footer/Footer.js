b32b42a37c6d67a543f7ee6c1b94c40f292ce15d
Parth Tyagi <parthtyagicode@gmail.com>
2022-12-28 09:40:48 +0530

    added linkedin

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { SiGithub } from 'react-icons/si';
+import { FaLinkedin } from 'react-icons/fa';
 import './Footer.css';
 
 export default function Footer() {
@@ -7,9 +8,18 @@ export default function Footer() {
         <footer>
             <div className="footer-container">
                 <h4><span>Developed By :</span> Parth Tyagi</h4>
-                <a href="https://github.com/parthtyagii/BlogApp">
-                    <SiGithub id="footer-github" />
-                </a>
+                <ul>
+                    <li>
+                        <a href="https://github.com/parthtyagii/BlogApp">
+                            <SiGithub className="footer-icons" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/parth-tyagi-0724a81b7/">
+                            <FaLinkedin className='footer-icons' />
+                        </a>
+                    </li>
+                </ul>
             </div>
         </footer>
     );
